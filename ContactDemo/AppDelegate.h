f533ce11fd57c9ff4f5b82e8913c919e66f23339
Tom Arra <tarra3@gmail.com>
2014-10-08 16:13:13 -0500

    Initial Commit

---
+++ ContactDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContactDemo
+//
+//  Created by Tom Arra on 10/8/14.
+//  Copyright (c) 2014 tomarra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ContactDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContactDemo
+//
+//  Created by Tom Arra on 10/8/14.
+//  Copyright (c) 2014 tomarra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
